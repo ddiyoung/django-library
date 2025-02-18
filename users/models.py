5f2b8e454c2fdb71bca9e2ea27e8be43cefaf7a6
ddiyoung <wsw3998@sju.ac.kr>
2025-02-19 01:06:29 +0900

    fix : Group과 Permission에 대해서 related_name 변경해야함

--- users/models.py
+++ users/models.py
@@ -1,16 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 
 
 class User(AbstractUser):
-    full_name = models.CharField(max_length=255, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    user_type = models.CharField(
-        max_length=10,
-        choices=[("customer", "Customer"), ("author", "Author"), ("admin", "Admin")],
-        default="customer",
+    full_name = models.CharField(max_length=255, blank=True, verbose_name="전체 이름")
+    phone_number = models.CharField(
+        max_length=20, blank=True, null=True, verbose_name="전화번호"
     )
+    address = models.TextField(blank=True, null=True, verbose_name="주소")
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name="생년월일")
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # 기본 'user_set'과 충돌을 피하기 위해 변경
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",  # 기본 'user_set'과 충돌을 피하기 위해 변경
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "user"
+        verbose_name = "User"
