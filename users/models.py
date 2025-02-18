0ba842cff9c55967800d76e5cfa4b628c9a3772e
ddiyoung <wsw3998@sju.ac.kr>
2025-02-19 00:28:21 +0900

    feature : 서점 서비스 User 모델 정의

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    user_type = models.CharField(
+        max_length=10,
+        choices=[("customer", "Customer"), ("author", "Author"), ("admin", "Admin")],
+        default="customer",
+    )
