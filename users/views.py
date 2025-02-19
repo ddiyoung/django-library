ffdaa6217aab34bab4bae05529f3912827ca0d56
ddiyoung <wsw3998@sju.ac.kr>
2025-02-19 18:05:32 +0900

    fix : SignIn Response 반환 안하던 이슈

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.generics import GenericAPIView, CreateAPIView
 from django.contrib.auth import login, authenticate
+from rest_framework.response import Response
 
 from users.serializer import SignInSerializer, SignUpSerializer
 
@@ -20,14 +20,15 @@ class SignInView(GenericAPIView):
 
         user = authenticate(
             request,
-            username=serializer.valid_data["username"],
-            password=serializer.valid_data["password"],
+            username=serializer.validated_data["username"],
+            password=serializer.validated_data["password"],
         )
 
         if not user:
             raise AuthenticationFailed("아이디 또는 비밀번호가 일치하지 않습니다.")
 
         login(request, user)
+        return Response()
 
 
 class SignUpView(CreateAPIView):
