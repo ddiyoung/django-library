52f32d476bfc4b5e4e6b27225d6acf8ed047221b
ddiyoung <wsw3998@sju.ac.kr>
2025-02-19 18:06:01 +0900

    fix : Custom User Group 및 permission 연결

--- users/models.py
+++ users/models.py
@@ -12,16 +12,8 @@ class User(AbstractUser):
     address = models.TextField(blank=True, null=True, verbose_name="주소")
     date_of_birth = models.DateField(blank=True, null=True, verbose_name="생년월일")
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name="custom_user_set",  # 기본 'user_set'과 충돌을 피하기 위해 변경
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="custom_user_permissions_set",  # 기본 'user_set'과 충돌을 피하기 위해 변경
-        blank=True,
-    )
+    groups = models.ManyToManyField(Group, blank=True)
+    user_permissions = models.ManyToManyField(Permission, blank=True)
 
     class Meta:
         db_table = "user"
